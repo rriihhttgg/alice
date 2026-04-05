6042e233766af5175528736e901eb2b8fb387c9b
rriihhttgg <adilhitting@gmail.com>
2026-04-06 00:54:10 +0600

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__)
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
 
 def log(msg):
